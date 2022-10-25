9d50212f5d72f62e727ebddce845c3c3db0bcd15
Md Al Amin <fpalamin6@gmail.com>
2022-10-25 21:06:08 +0600

    data loded

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 
-
-
+const allCourse =require('./data/education-data.json')
 app.get('/', (req, res) => {
     res.send('Education server Running');
+});
+
+app.get('/allcourse', (req, res) => {
+    res.send(allCourse);
 });
 
 app.listen(port, () => {
